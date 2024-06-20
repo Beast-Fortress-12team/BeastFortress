d7e3ee109f5e2a1e54dd6bdaedd4d71acbbe3b48
JaeEun18 <ban5446@naver.com>
2024-06-20 11:58:43 +0900

    mrege 확인용

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("이거 확인용 추가추가추가");  
     }
 
     // Update is called once per frame
